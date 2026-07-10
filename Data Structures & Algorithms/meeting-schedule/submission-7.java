050e7a921f275d546dace867673e09399654db90
Maleficent <144607072+SimonaPrince@users.noreply.github.com>
2026-07-10 10:04:07 +0530

    Add: meeting-schedule - submission-7

---
+++ Data Structures & Algorithms/meeting-schedule/submission-7.java
@@ -0,0 +1,44 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+       int[] start=new int[intervals.size()];
+       int[] end=new int[intervals.size()];
+        for(int i=0;i<intervals.size();i++){
+            start[i]=intervals.get(i).start;
+        }
+        for(int i=0;i<intervals.size();i++){
+            end[i]=intervals.get(i).end;
+        }
+       Arrays.sort(start);
+       Arrays.sort(end);
+       int i=0;
+       int j=0;
+       int count=0;
+       int res=0;
+       while(i < start.length){
+           if(start[i] < end[j]){
+              count++;
+              i++;
+           }
+           else if(start[i] >= end[j]){
+               count--;
+               j++;
+           }
+           res=Math.max(res,count);
+       }
+       if(res <=1){
+           return true;
+       }
+       return false;
+    }
+}
